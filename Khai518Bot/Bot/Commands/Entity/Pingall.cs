11f4b56f782c9965e280daf110426147e2a4af5e
Andrey <andreypopov2628007@gmail.com>
2022-12-21 11:17:17 +0200

    Fix pingall

--- Khai518Bot/Bot/Commands/Entity/Pingall.cs
+++ Khai518Bot/Bot/Commands/Entity/Pingall.cs
@@ -10,24 +10,11 @@ namespace Khai518Bot.Bot.Commands.Entity;
 
 public class PingAll : Command
 {
-    private async Task<string> ToPing(long chatId)
-    {
-        try
-        {
-            var users = await _client.GetChatAdministratorsAsync(chatId);
-            var ans = users.Where(x => !x.User.IsBot).Select(x => $"[.](tg://user?id={x.User.Id})")
-                .Aggregate((x, y) => x + y);
-            return ans;
-        }
-        catch (Exception)
-        {
-            return "";
-        }
-    }
     public override async Task Execute(Service service)
     {
-        var chat = Message.Chat.Id;
-        var toPing = await ToPing(chat);
-        await BotClient.SendTextMessageAsync(chat, $"Рубель зовёт Вас!{toPing}");
+        var users = await BotClient.GetChatAdministratorsAsync(Message.Chat);
+        var toPing = users.Where(x => !x.User.IsBot).Select(x => $"[.](tg://user?id={x.User.Id})")
+            .Aggregate((x, y) => x + y);
+        await BotClient.SendTextMessageAsync(Message.Chat, $"Рубель зовёт Вас!{toPing} (макс где вебхуки)", ParseMode.Markdown);
     }
 }
